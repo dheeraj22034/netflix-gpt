26568cfea1545e913b8e3c176bb27bb4cc69fa2f
sree <sanagani22.yadav@gmail.com>
2025-09-02 14:20:57 +0530

    modified

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="font-bold ">
+      <header className="App-header">
+       
+        <p className='text-green-300'>
+          hi dheeraj
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
